434414541c78468ba04086f23589547df911bdaf
PadjokeJ <jonatan@padjokej.dev>
2025-10-30 14:34:43 +0100

    Small cleanup in challenge codebase

--- src/main/java/ch/epfl/cs107/Challenge.java
+++ src/main/java/ch/epfl/cs107/Challenge.java
@@ -11,7 +11,8 @@ import java.nio.charset.StandardCharsets;
  * <b>06</b> Provided for you to attempt the challenge in
  *
  * @author Mehdi ALAOUI (mehdi.alaoui@epfl.ch)
- * @version 1.0.0
+ * @author Solved by Jonatan PFISTER (jonatan.pfister@epfl.ch)
+ * @version 2.0.0
  * @since 1.0.0
  */
 public class Challenge {
@@ -44,49 +45,47 @@ public class Challenge {
         System.out.println(Text.toString(decrypted));
         
         /* We find 0x37 as a hint */ 
-        /* Let's decrypt the third hint with 0x37 and diverse algs */
+        /* Let's decrypt the third hint with 0x37 and xor */
 
         byte[] hint3 = Helper.read("challenge/hint3.txt");
         byte[] hi3decrypted = Decrypt.xor(hint3, (byte) 0x37);
-        //System.out.println(Text.toString(hi3decrypted));
+        System.out.println(Text.toString(hi3decrypted));
         /* KEYWORD=c4Ptur37hEfL46; IV_POS=120..136 */
+        
+        /* Let's extract the keyword */
+        String hint3Str = Text.toString(hi3decrypted);
+        String hint3KeywordStr = hint3Str.split("; ")[0].split("=")[1];
+        //System.out.println(hint3KeywordStr); --> "c4Ptur37hEfL46"
 
         /* We find a keyword and a IV position */
         /* Let's decrypt images */
         int[][] image1 = Helper.readImage("challenge/image1.png");
         byte[] textimage1 = TextSteganography.revealText(image1);
-        byte[] image1decrypted = Decrypt.vigenere(textimage1, Text.toBytes("c4Ptur37hEfL46"));
+        /* A keyword is the technical term for the key in vigenere, so let's use that */
+        byte[] image1decrypted = Decrypt.vigenere(textimage1, Text.toBytes(hint3KeywordStr));
         System.out.println(Text.toString(image1decrypted).substring(120, 136));
         
+        // Let's truncate out the "IV:"
         byte[] posBytes1 = new byte[IV_LEN - 3];
         for (int i = 3; i < IV_LEN; i++) {
             int k = 120 + i;
             posBytes1[i - 3] = image1decrypted[k];
         }
-        System.out.println(Text.toString(posBytes1));
         
-        byte[] posBytes = Text.toBytes("UV9L2k!dA4rT0");
+        byte[] posBytes = posBytes1; //Text.toBytes("UV9L2k!dA4rT0");
         
-        int j = 0;
-        //assert posBytes.length == posBytes1.length;
-        for (byte i: posBytes) {
-            System.out.println(posBytes1[j] + " : " + i);
-            //assert i == (posBytes1[j]);
-            
-            j++;
-        }
-
+        /* Let's reveal the second image */
         int[][] image2 = Helper.readImage("challenge/image2.png");
         byte[] textimage2 = TextSteganography.revealText(image2);
-        //textimage2[0] -= 32;
-        System.out.println(textimage2[0]);
         
+        /* and IV is the technical term for the key in CBC, so we can use that to solve it */
         byte[] image2decrypted = Decrypt.cbc(textimage2, posBytes1);
         String solString = Text.toString(image2decrypted);
-        System.out.println(Text.toString(Decrypt.cbc(textimage2, posBytes1)).substring(0, 100));
-
+        
+        /* Let's truncate out the flag until the ending curly bracket */
         int posMax = solString.indexOf('}') + 1;
         System.out.println(Text.toString(image2decrypted).substring(0, posMax));
+        /* We find the flag FLAG{C5-IO7;F0r743w1Nn} */
         return solString.substring(0, posMax);
     }
 
@@ -97,11 +96,6 @@ public class Challenge {
         for (int i = 0; i < text.length; i++) {
             counter[text[i] + 128] += 1;
         }
-
-        //for (int i = 0; i < counter.length; i++ ) {
-        //    if (counter[i] > 0)
-        //        System.out.println("-" + i + "\t:  " + counter[i]);
-        //}
 
         return counter;
     }
